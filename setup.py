10ebb8ef5b77e406f7c95c50cd074eeb43c81879
Disant Upadhyay <disantupadhyay07@gmail.com>
2023-05-17 18:28:05 -0230

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="find_old_large_files",
-    version="0.1",
+    version="0.1.1",  # Increment the version here
     packages=find_packages(),
     entry_points={
         'console_scripts': [
